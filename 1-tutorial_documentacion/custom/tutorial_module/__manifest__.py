1f9f2f8c85a0ead3548d4a1cde1718b659026c2a
Marco Ruiz <martulioruiz@gmail.com>
2021-10-16 23:44:18 -0600

    CH3 -> Una nueva aplicacion
    1. Dentro de la carpeta "tutorial_module", se crearon los archivos init y manifest con los datos justos para que el framework de odoo pueda reconocerla como un nuevo modulo

---
+++ 1-tutorial_documentacion/custom/tutorial_module/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Tutorial Module',
+    'version': '1.0',
+    'category': 'Hidden',
+    'description': """Modulo de prueba creado a partir de la documentacions de odoo 15""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
