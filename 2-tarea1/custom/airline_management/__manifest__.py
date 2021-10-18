4997ce23a35b8ab9df15b6a52933f602a828aa73
Marco Ruiz <martulioruiz@gmail.com>
2021-10-17 18:21:48 -0600

    Estructura inicial del modulo
    1.Se crearon los archivos escenciales para que el framework de odoo pueda reconocer el modulo.

---
+++ 2-tarea1/custom/airline_management/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Airline management',
+    'version': '1.0',
+    'category': 'Hidden',
+    'description': """Modulo para el control de actividades basicas en una aerolinea""",
+    'website': 'https://www.odoo.com/app/airline',
+    'depends': ['base_setup'],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
